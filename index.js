5eebee3775f22dc5df3fa81d7a2aa132ea2c2eaa
Brenn Kucey <bkucey@newrelic.com>
2022-07-05 21:55:53 -0700

    add click handlers

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Hello");
+
+function handleClick(event) {
+  console.log("tile clicked");
+  console.log(event.target);
+}
+
+const tiles = document.querySelectorAll(".tile");
+tiles.forEach((tile) => {
+  tile.addEventListener("click", handleClick);
+});
